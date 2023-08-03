767d364a8f492d76c74b2a014420f04387255b7f
TrevSh <trevonshelby@outlook.com>
2023-08-03 03:01:15 -0500

    Added single coin functionality

--- SSBS1_PenniesAndDollars/SSBS1_PenniesAndDollars.cpp
+++ SSBS1_PenniesAndDollars/SSBS1_PenniesAndDollars.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 double pennies = 0.01;
-double nickles = 0.05;
+double nickels = 0.05;
 double dimes = 0.10;
 double quarters = 0.25;
 double half_dollar = 0.50;
@@ -13,11 +13,11 @@ int main()
 {
     char again = 0;
     do {
-    int numPennies, numNickles, numDimes, numQuarters, numHalfs;
+    int numPennies, numNickels, numDimes, numQuarters, numHalfs;
     std::cout << "\nHow many pennies do you have?\n";
     std::cin >> numPennies;
     std::cout << "How many nickles do you have?\n";
-    std::cin >> numNickles;
+    std::cin >> numNickels;
     std::cout << "How many dimes do you have?\n";
     std::cin >> numDimes;
     std::cout << "How many quarters do you have?\n";
@@ -25,14 +25,29 @@ int main()
     std::cout << "How many half dollars do you have?\n";
     std::cin >> numHalfs;
     
-    double value = (pennies * numPennies) + (nickles * numNickles) + (dimes * numDimes) + (quarters * numQuarters) + (half_dollar * numHalfs);
-    if (value <= 1.00) {
+    double value = (pennies * numPennies) + (nickels * numNickels) + (dimes * numDimes) + (quarters * numQuarters) + (half_dollar * numHalfs);
+    if (value == 0.01) {
+        std::cout << "You have one penny.\n";
+    }
+    else if (value == 0.05) {
+        std::cout << "You have one nickel.\n";
+    }
+    else if (value == 0.10) {
+        std::cout << "You have one dime.\n";
+    }
+    else if (value == 0.25) {
+        std::cout << "You have one quarter.\n";
+    }
+    else if (value == 0.50) {
+        std::cout << "You have one half dollar... Lucky you!\n";
+    }
+    else if (value <= 1.00) {
         std::cout << "You have " << value << " cents.\n";
     }
     else {
         std::cout << "You have $" << value<<"\n";
     }
-    std::cout << "Again? (y/n): ";
+    std::cout << "\nAgain? (y/n): ";
     std::cin >> again;
     } while (again == 'y' || again == 'Y');
 }
